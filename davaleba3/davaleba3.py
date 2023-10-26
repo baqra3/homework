23934c0ad9700429cfb8b6856099ea69d3361a98
baqra3 <148813533+baqra3@users.noreply.github.com>
2023-10-26 23:58:49 +0400

    Add files via upload

---
+++ davaleba3/davaleba3.py
@@ -0,0 +1,14 @@
+My_age = 18
+Sister_age = 17
+Mom_age = 50
+Dad_age = 55
+
+My_age += 5
+Sister_age += 5
+Mom_age += 5
+Dad_age += 5
+
+print(My_age)
+print(Sister_age)
+print(Mom_age)
+print(Dad_age)
