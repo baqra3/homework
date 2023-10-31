f69f7dd3151284bd19424ce88c7101364b602c0b
baqra3 <148813533+baqra3@users.noreply.github.com>
2023-11-01 01:38:49 +0400

    Add files via upload

---
+++ davaleba5.py
@@ -0,0 +1,6 @@
+my_age = int(input("Enter Your Age: "))
+fathers_age = int(input("Enter Your Fathers Age:"))
+year = int(input("Enter Year: "))
+
+for i in range(30):
+    print(str(year + i )+ " წელს მამაჩემი ჩემზე იქნება " + str(fathers_age + i - my_age) + "წლით დიდი")
