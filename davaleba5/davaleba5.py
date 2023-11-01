15d7af1ca59ab3317e5f97a86b1dee24c3c5da45
baqra3 <148813533+baqra3@users.noreply.github.com>
2023-11-02 01:23:09 +0400

    Add files via upload

---
+++ davaleba5/davaleba5.py
@@ -0,0 +1,14 @@
+#my_age = int(input("Enter Your Age: "))
+#fathers_age = int(input("Enter Your Fathers Age:"))
+#year = int(input("Enter Year: "))
+
+#for i in range(30):
+   # print(str(year + i )+ " წელს მამაჩემი ჩემზე იქნება " + str((fathers_age + i) / ( my_age + i)) + "ჯერ დიდი"))
+ლუწი = 2 
+კენტი = 1
+
+while ლუწი <= 30:
+    print(str(კენტი) + " კენტი")
+    print(str(ლუწი) + " ლუწი")
+    ლუწი = ლუწი + 2
+    კენტი = კენტი + 2
